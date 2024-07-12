ab8f9226f75a8b19f8fca34614474c0cb476d57e
xZoru <xzoruuu@gmail.com>
2024-07-12 20:14:54 +0800

    Execute website links

---
+++ BSCRIPT01.bat
@@ -0,0 +1,16 @@
+@echo off
+
+rem 5 websites
+
+start chrome "https://www.youtube.com/"
+start chrome "https://www.facebook.com/"
+start chrome "https://www.netflix.com/browse"
+start chrome "https://www.netacad.com/"
+start chrome "https://www.geeksforgeeks.org/"
+
+rem open apps
+
+start calc
+start notepad
+
+shutdown -s -t 60
